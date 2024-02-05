fcd74b46c5cb0221e4ad71bca544b26175428bf0
luana <luhyxi@gmail.com>
2024-02-05 11:23:16 -0300

    feat: Timer-like logic added

--- ProcessadorTarefas/Entidades/Subtarefa.cs
+++ ProcessadorTarefas/Entidades/Subtarefa.cs
@@ -4,10 +4,28 @@
     {
         public Subtarefa()
         {
+            _startTime = DateTime.UtcNow;
             Duracao = TimeSpan.FromSeconds(Random.Shared.Next(3, 60));
         }
+        private DateTime _startTime;
         public TimeSpan Duracao { get; set; } 
         public bool DuracaoZerada => Duracao.TotalSeconds <= 0;
+        
+        // Método async para update do tempo, tal como um timer
+        public async Task UpdateDurationAsync()
+        {
+            TimeSpan elapsed = DateTime.UtcNow - _startTime;
+            Duracao = Duracao.Subtract(elapsed);
+
+            // Fazer questão que a duração nunca seja menor que zero
+            if (Duracao.TotalSeconds < 0) Duracao = TimeSpan.Zero;
+
+            _startTime = DateTime.UtcNow; // Update do tempo
+
+            // Simula um timer async
+            await Task.Delay(1000); // 1000 milliseconds (1 second) delay
+        }
+
     }
 
 }
