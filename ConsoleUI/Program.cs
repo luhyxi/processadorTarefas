35e6e40e6f166fe13531ce4005018a9a13c52a7f
luana <luhyxi@gmail.com>
2024-02-09 12:48:48 -0300

    feat: started working on ConsoleUI

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,11 +1,1 @@
-﻿namespace SOLID_Example
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello world!");
-        }
-
-    }
-}
+﻿Console.WriteLine("Hello world!");
